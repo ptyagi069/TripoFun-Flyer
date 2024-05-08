d08b363d3c42f2adcad52c5481169255ec794a55
ptyagi069 <tyagiprateek69@gmail.com>
2024-05-08 17:51:12 +0530

    123456789

--- script.js
+++ script.js
@@ -12,14 +12,14 @@ function createContainerHTML(service, index) {
               <div class="activity">
                 <h5>Activity ${index + 1}</h5>
                 <div class="addon-info">
-                  <p style="font-size: 26px; font-weight: 500; line-height: 26px; width: 70%;">${service.servicE_NAME}</p>
-                  <span style="color: #00ACEA; font-weight: 700; float: right;">@ $${service.guestAmount} only</span>
+                  <p style="font-size: 22px; font-weight: 500; line-height: 26px; width: 50%;float: left;">${service.servicE_NAME}</p>
+                  <span style="color: #00ACEA; font-weight: 700;  font-size: 26px; float: right;">@ $${service.guestAmount} only</span>
                 </div>
               </div>
               <div class="time-days">
-                <p style="font-weight: 700;"><img src="images/clock.png" alt="" style="width: 30px; height: 30px; margin-right: 5px;"> ${service.duration}</p>
-                <p style="font-weight: 700;"><img src="images/sunset.png" alt="" style="width: 30px; height: 30px; margin-right: 5px;">add on Day ${service.nO_OF_DAYS}</p>
-              </div>
+              <p style="font-weight: 700;"><img src="images/clock.png" alt="" style="width: 30px; height: 30px; margin-right: 5px;"> ${service.duration}</p>
+              <p style="font-weight: 700;"><img src="images/sunset.png" alt="" style="width: 30px; height: 30px; margin-right: 5px;">add on Day ${service.nO_OF_DAYS}</p>
+            </div>
               <div class="content">
                 <p style="font-size: 18px; font-weight: 400; line-height: 21px; padding: 10px;">${service.servicE_DESCRIPTION}</p>
               </div>
@@ -34,9 +34,9 @@ function createContainerHTML(service, index) {
             <div class="col-md-8" style="border: 0.5px solid #ECECEC; border-radius: 20px; background-color: #F9F9F9;">
               <div class="activity">
                 <h5>Activity ${index + 1}</h5>
-                <div class="addon-info" style="display: flex;">
-                  <p style="font-size: 26px; font-weight: 500; line-height: 26px;">${service.servicE_NAME}</p>
-                  <span style="color: #00ACEA; font-weight: 700; line-height: 26px;">@ $${service.guestAmount} only</span>
+                <div class="addon-info" >
+                  <p style="font-size: 22px; font-weight: 500; line-height: 26px; width: 50%; float:left;">${service.servicE_NAME}</p>
+                  <span style="color: #00ACEA; font-weight: 700; line-height: 26px; float : right;  font-size: 26px;">@ $${service.guestAmount} only</span>
                 </div>
               </div>
               <div class="time-days">
@@ -58,7 +58,7 @@ function createContainerHTML(service, index) {
   
   function fetchAddOnsDetails() {
     const apiUrl = 'https://mobileapi.cultureholidays.com/api/Holidays/GetAddOnsDetails';
-    const requestData = { agentID: 'CHAGT0001000012263', pkG_ID: '8', year: '2024' };
+    const requestData = { agentID: 'CHAGT000004000', pkG_ID: '9', year: '2024' };
   
     fetch(apiUrl, {
       method: 'POST',
@@ -79,9 +79,7 @@ function createContainerHTML(service, index) {
       const containerSection = document.querySelector('#dynamic-containers');
       containerSection.innerHTML = '';
   
-      const dataLength = data.length; 
-      console.log('Data Length:', dataLength);
-  
+      const dataLength = data.length;   
       for (let i = 0; i < dataLength; i++) {
         const service = data[i]; 
   
@@ -98,3 +96,22 @@ function createContainerHTML(service, index) {
 
 
   
+  function downloadPoster() {
+    const posterDiv = document.querySelector('.a4-wrapper');
+
+    html2canvas(posterDiv).then(canvas => {
+        const dataUrl = canvas.toDataURL('image/png');
+
+        const downloadLink = document.createElement('a');
+        downloadLink.href = dataUrl;
+        downloadLink.download = 'poster.png'; 
+
+        document.body.appendChild(downloadLink);
+        downloadLink.click();
+
+        document.body.removeChild(downloadLink);
+    });
+}
+
+const downloadBtn = document.getElementById('downloadPosterBtn');
+downloadBtn.addEventListener('click', downloadPoster);
